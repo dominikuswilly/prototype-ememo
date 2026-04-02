faf0c4255285a6b8e2197cf02b0d4ce926f06fda
willy <willy@kbru.co.id>
2026-04-02 15:25:57 +0700

    adjust: all categories

--- src/modules/presentations/components/PresentationFilter.vue
+++ src/modules/presentations/components/PresentationFilter.vue
@@ -275,7 +275,7 @@ defineExpose({ focusSearch });
 }
 
 .select-input {
-  padding-left: 0.85rem;
+  padding-left: 2.5rem;
   appearance: none;
   background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='%2394a3b8'%3E%3Cpath stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M19 9l-7 7-7-7'%3E%3C/path%3E%3C/svg%3E");
   background-repeat: no-repeat;
